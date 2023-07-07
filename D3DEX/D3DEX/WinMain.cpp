81f4145efbc6d1a5919acbc9f6da4867870c7246
Lcat <ljj7403@naver.com>
2023-07-07 21:28:02 +0900

    tuto 10

--- D3DEX/D3DEX/WinMain.cpp
+++ D3DEX/D3DEX/WinMain.cpp
@@ -17,7 +17,7 @@
 *	You should have received a copy of the GNU General Public License					  *
 *	along with The Chili Direct3D Engine.  If not, see <http://www.gnu.org/licenses/>.    *
 ******************************************************************************************/
-#include "Window.h"
+#include "App.h"
 #include <sstream>
 
 
@@ -29,52 +29,7 @@ int CALLBACK WinMain(
 {
 	try 
 	{
-		Window wnd(800, 300, L"³¢¿ê");
-
-		MSG msg;
-		BOOL gResult;
-		while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
-		{
-			// TranslateMessage will post auxilliary WM_CHAR messages from key msgs
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-			
-			// test code
-			static int i = 0;
-			while (!wnd.mouse.IsEmpty())
-			{
-				const auto e = wnd.mouse.Read();
-				switch (e.GetType())
-				{
-				case Mouse::Event::Type::WheelUp:
-					i++;
-					{
-						std::ostringstream oss;
-						oss << "Up: " << i;
-						wnd.SetTitle(oss.str());
-					}
-					break;
-				case Mouse::Event::Type::WheelDown:
-					i--;
-					{
-						std::ostringstream oss;
-						oss << "Down: " << i;
-						wnd.SetTitle(oss.str());
-					}
-					break;
-				}
-			}
-
-		}
-
-		// check if GetMessage call itself borked
-		if (gResult == -1)
-		{
-			return -1;
-		}
-
-		// wParam here is the value passed to PostQuitMessage
-		return msg.wParam;
+		return App{}.Go();
 	}
 	catch (const ChiliException& e)
 	{
