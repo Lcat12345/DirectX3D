fff11c51d92586100eaab44e74a00415fc88a9ba
Lcat <ljj7403@naver.com>
2023-07-23 22:43:57 +0900

    ㅠㅠ

--- D3DEX/D3DEX/Window.h
+++ D3DEX/D3DEX/Window.h
@@ -31,16 +31,30 @@ class Window
 public:
 	class Exception : public ChiliException
 	{
+		using ChiliException::ChiliException;
 	public:
-		Exception(int line, const char* file, HRESULT hr) noexcept;
-		const char* what() const noexcept override;
-		virtual const char* GetType() const noexcept;
 		static std::string TranslateErrorCode(HRESULT hr) noexcept;
+	};
+
+	class HrException : public Exception
+	{
+	public:
+		HrException(int line, const char* file, HRESULT hr) noexcept;
+		const char* what() const noexcept override;
+		const char* GetType() const noexcept override;
 		HRESULT GetErrorCode() const noexcept;
-		std::string GetErrorString() const noexcept;
+		std::string GetErrorDescription() const noexcept;
 	private:
 		HRESULT hr;
 	};
+
+	class NoGfxException : public Exception
+	{
+	public:
+		using Exception::Exception;
+		const char* GetType() const noexcept override;
+	};
+
 private:
 	// singleton manages registration/cleanup of window class
 	class WindowClass
@@ -53,7 +67,7 @@ private:
 		~WindowClass();
 		WindowClass(const WindowClass&) = delete;
 		WindowClass& operator=(const WindowClass&) = delete;
-		static constexpr LPCWSTR wndClassName = L"�����ڵ� �ں���";
+		static constexpr LPCWSTR wndClassName = L"Lcat";
 		static WindowClass wndClass;
 		HINSTANCE hInst;
 	};
@@ -63,7 +77,7 @@ public:
 	Window(const Window&) = delete;
 	Window& operator=(const Window&) = delete;
 	void SetTitle(const std::string& title);
-	static std::optional<int> ProcessMessages();
+	static std::optional<int> ProcessMessages() noexcept;
 	Graphics& Gfx();
 
 private:
@@ -83,5 +97,6 @@ private:
 };
 
 // error exception helper macro
-#define CHWND_EXCEPT( hr ) Window::Exception( __LINE__,__FILE__,hr )
-#define CHWND_LAST_EXCEPT() Window::Exception( __LINE__,__FILE__,GetLastError() )
+#define CHWND_EXCEPT( hr ) Window::HrException( __LINE__,__FILE__,(hr) )
+#define CHWND_LAST_EXCEPT() Window::HrException( __LINE__,__FILE__,GetLastError() )
+#define CHWND_NOGFX_EXCEPT() Window::NoGfxException( __LINE__,__FILE__ )
