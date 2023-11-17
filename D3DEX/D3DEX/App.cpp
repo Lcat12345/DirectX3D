45b21ab18ff62345774ce53fd9bd85dd30560f20
Lcat <ljj7403@naver.com>
2023-11-17 21:57:42 +0900

    tuto 25 end
    Finally Success Load Suzan

--- D3DEX/D3DEX/App.cpp
+++ D3DEX/D3DEX/App.cpp
@@ -3,6 +3,7 @@
 #include "Cylinder.h"
 #include "Pyramid.h"
 #include "SkinnedBox.h"
+#include "AssTest.h"
 #include <memory>
 #include <algorithm>
 #include "ChiliMath.h"
@@ -68,12 +69,6 @@ App::App()
 		std::uniform_int_distribution<int> typedist{ 0, 2 };
 	};*/
 
-	Assimp::Importer imp;
-	auto model = imp.ReadFile("models\\suzanne.obj",
-		aiProcess_Triangulate |
-		aiProcess_JoinIdenticalVertices
-	);
-
 	class Factory
 	{
 	public:
@@ -107,6 +102,11 @@ App::App()
 					gfx, rng, adist, ddist,
 					odist, rdist
 				);
+			case 4:
+				return std::make_unique<AssTest>(
+					gfx, rng, adist, ddist,
+					odist, rdist, mat, 1.5f
+				);
 			default:
 				assert(false && "impossible drawable option in factory");
 				return {};
@@ -115,7 +115,7 @@ App::App()
 	private:
 		Graphics& gfx;
 		std::mt19937 rng{ std::random_device{}() };
-		std::uniform_int_distribution<int> sdist{ 0, 3 };
+		std::uniform_int_distribution<int> sdist{ 0, 4 };
 		std::uniform_real_distribution<float> adist{ 0.0f, PI * 2.0f };
 		std::uniform_real_distribution<float> ddist{ 0.0f, PI * 0.5f };
 		std::uniform_real_distribution<float> odist{ 0.0f, PI * 0.08f };
